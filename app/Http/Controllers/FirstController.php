b1a4498dc63c2e111341715f17a54de8d05f8731
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-06 15:25:33 +0300

    Testing the link between controller & api

--- app/Http/Controllers/FirstController.php
+++ app/Http/Controllers/FirstController.php
@@ -5,5 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class FirstController extends Controller{
-    //
+    
+    public function sayHello(){
+        echo "Hello World!";
+    }
+
 }
