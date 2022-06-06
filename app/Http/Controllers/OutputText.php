caf682024f7f7d4c0b77614a7f2945ebac68816c
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-06 17:59:41 +0300

    Working on retreiving text from an api

--- app/Http/Controllers/OutputText.php
+++ app/Http/Controllers/OutputText.php
@@ -8,8 +8,14 @@ class OutputText extends Controller{
     
     public function outputTextField(){
 
-        echo "Text!";
-        
+        // Not completed
+        $url = "https://icanhazdadjoke.com/slack";
+        // open("https://icanhazdadjoke.com/slack");
+        // // window.open("https://icanhazdadjoke.com/slack");
+        // $content = parse_url($url);
+        $content = '<script type="text/javascript">window.open("https://icanhazdadjoke.com/slack");</script>';
+        $content =  json_encode($content);
+        echo $content;
     }
 
 }
