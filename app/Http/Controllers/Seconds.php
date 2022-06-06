dc8399f10b5f2068e24994461db3e047024ce703
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-06 17:16:02 +0300

    Implementing a function that calculates how many seconds passed since 14 April 1732

--- app/Http/Controllers/Seconds.php
+++ app/Http/Controllers/Seconds.php
@@ -7,8 +7,17 @@ use Illuminate\Http\Request;
 class Seconds extends Controller{
 
     public function secondsNumber(){
-        echo "Seconds";    
-    
+
+        // Number of seconds from January 1 1970 till the current moment
+        $seconds_since_1970 = strtotime("now");
+
+        // Number of seconds from 14 April 1732 till January 1 1970
+        $seconds_from_1732_till_1970 = strtotime("14 April 1732");
+
+        // Total number of seconds from 14 April 1732 till current moment
+        $required_number_of_seconds = $seconds_since_1970 - $seconds_from_1732_till_1970;
+        echo "The number of seconds since 14 April 1732 is equal to " . $required_number_of_seconds;
+        
     }
 
 }
