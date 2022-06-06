6e21b189ad91fd95264cd73ea1354b9850323c5e
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-06 16:40:11 +0300

    Implementing the checkPalindromeNumber function

--- app/Http/Controllers/Palindrome.php
+++ app/Http/Controllers/Palindrome.php
@@ -7,7 +7,21 @@ use Illuminate\Http\Request;
 class Palindrome extends Controller{
     
     public function checkPalindromeNumber(){
-        echo "Test";
-    }
 
+        $palindrome_counter = 0;
+        $strings = array("aaabaaa","abe","ccABBAd","XYZZYX","eaaabaaae");
+        
+        // Looping over each string in the array and checking if it is palindrome
+        // If yes, increment the counter
+        for($x = 0; $x < count($strings); $x++) {
+
+            if ($strings[$x] == strrev($strings[$x])){
+                $palindrome_counter++;
+            }
+
+        }
+
+        echo "The number of palindromes in the pre-defined array is equal to " . $palindrome_counter;
+
+    }
 }
